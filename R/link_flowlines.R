8b594cca0db86f364bd7ce9d7c31808bce1a031e
Luke Winslow <lawinslow@gmail.com>
2017-07-19 15:24:39 -0400

    handle edge case for flowlines linking

--- R/link_flowlines.R
+++ R/link_flowlines.R
@@ -6,7 +6,7 @@
 #' @param lons Vector of point longitudes
 #' @param ids Vector of point identifiers (string or numeric)
 #' @param max_dist numeric maximum line snapping distance in meters
-#' 
+#'
 #' @return flowline permanent ids
 #'
 #' @import rgdal
@@ -20,7 +20,7 @@ link_flowlines = function(lats, lons, ids, max_dist = 100){
   bbdf_streams = NULL
   load(file=system.file('extdata/nhd_bb_streams_cache.Rdata', package='nhdtools'))
   wbd_bb = bbdf_streams
-  
+
   sites = data.frame(lats, lons, ids)
   xy = cbind(sites$lons, sites$lats)
   not_na = which(!is.na(sites$lats) & !is.na(sites$lons))
@@ -28,42 +28,48 @@ link_flowlines = function(lats, lons, ids, max_dist = 100){
   ids.not_na..drop...FALSE. = NULL
   pts@data = rename(pts@data, MATCH_ID = ids.not_na..drop...FALSE.)
   pts = spTransform(pts, CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs"))
-  
+
   res   = list()
-  
+
   matches_found = 0
-  
+
   xmin = xmax = ymin = ymax = NULL
-  
+
   for(i in 1:nrow(pts@coords)){
     res[[i]] = subset(wbd_bb, xmin <= pts@coords[i,1] & xmax >= pts@coords[i,1] & ymin <= pts@coords[i,2] & ymax >= pts@coords[i,2])
   }
-  
+
   to_check = unique(do.call(rbind, res))
-  
-  
+
+
   match_res = list()
-  
+
   if(nrow(to_check) == 0){
     ret = data.frame(MATCH_ID = sites$ids)
     ret$PERMANENT_ID = NA
     return(ret)
   }
-  
+
   for(i in 1:nrow(to_check)){
     #get nhd layer
     check_dl_file(system.file("extdata/nhdh.csv", package = "nhdtools"), to_check[i, 'file'])
-    
+
     nhd       = readOGR(file.path(local_path(), "unzip", to_check[i,'file'], "NHDFlowline_projected.shp"))
-    
+
     ids = rep(NA, length(sites$lats))
-    
+
     bbox = wbd_bb[wbd_bb$file == to_check[i, 'file'], ]
     pts = pts[bbox$xmin <= pts@coords[, 1] & bbox$xmax >= pts@coords[,1] & bbox$ymin <= pts@coords[,2] & bbox$ymax >= pts@coords[,2],]
+
+    #if there are no points to check that overlap this shapefile, skip
+    if(nrow(pts) < 1){
+      next
+    }
+
     matches = snapPointsToLines(pts, nhd, maxDist = max_dist, idField = ids)
     if(is.na(matches)){
       match_res[i*nrow(pts@coords) -nrow(pts@coords) + j] = NA
-      next()
+      next
     }
     matches_found = matches_found + nrow(matches)
     for(j in 1:nrow(matches)){
@@ -72,10 +78,10 @@ link_flowlines = function(lats, lons, ids, max_dist = 100){
       match_res[[i*nrow(matches) - nrow(matches) + j]] = match_data
     }
     if(matches_found == nrow(sites)){
-      break()
+      break
     }
   }
-  
+
   unique_matches = unique(do.call(rbind, match_res))
   #return matches that have non-NA value PREMANENT_ID
   return(unique_matches[!is.na(unique_matches$PERMANENT_), ])
