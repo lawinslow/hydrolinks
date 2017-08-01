e0afd577a8df30f6d7867f2b735a794291623f08
Tobi Hahn <rainshapes@gmail.com>
2017-08-01 13:59:04 -0400

    added centroids to shapefiles

--- demo/project_waterbodies.R
+++ demo/project_waterbodies.R
@@ -1,4 +1,5 @@
 library(rgdal)
+library(rgeos)
 library(parallel)
 
 shapes = file.path(local_path(),"unzip", all_nhd_shapefiles())
@@ -6,12 +7,19 @@ shapes = file.path(local_path(),"unzip", all_nhd_shapefiles())
 project_waterbodies = function(waterbody){
   shape = readOGR(file.path(waterbody, "NHDWaterbody.shp"))
   shape = spTransform(shape, CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs"))
+  centroids = gCentroid(shape, byid = TRUE)
+  shape$centroid.x = centroids@coords[,"x"]
+  shape$centroid.y = centroids@coords[,"y"]
+  if(file.exists(file.path(waterbody, "NHDWaterbody_projected.shp"))){
+    file.remove(Sys.glob(file.path(waterbody, "NHDWaterbody_projected.*")))
+  }
   writeOGR(shape, dsn = waterbody, layer = "NHDWaterbody_projected", driver = "ESRI Shapefile")
   return(shape@bbox)
 }
 
 c1 = makePSOCKcluster(rep('localhost', 8))
-parallel::clusterEvalQ(c1, {library(rgdal)})
+parallel::clusterEvalQ(c1, {library(rgdal)
+  library(rgeos)})
 bboxes = parLapplyLB(c1, shapes, project_waterbodies)
 
 bbdf = data.frame(file=character(), xmin=numeric(), xmax=numeric(), ymin=numeric(), ymax=numeric(), stringsAsFactors = FALSE)
