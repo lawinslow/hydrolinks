b611ed9be2de4dee4ddc1a6d72211610600b6072
Tobi Hahn <rainshapes@gmail.com>
2017-08-21 13:36:58 -0400

    fixed traversing with direction in

--- R/traverse_flowlines.R
+++ R/traverse_flowlines.R
@@ -19,37 +19,32 @@
 #' }
 traverse_flowlines = function(g, distance, start, direction = c("out", "in")){
   direction = match.arg(direction)
-  nodes = c()
+  nodes = data.frame()
   n = neighbors(g, start, direction)
   to_check = n$LENGTHKM
   names(to_check) = n$ID
-  # to_check = apply(neighbors(g, start, direction), 1, function(x){
-  #   to_check = c(to_check, x["LENGTHKM"])
-  #   names(to_check) = c(names(to_check), x["ID"])
-  #   # if(!is.null(to_check[x["ID"]])){
-  #   #   to_check[x["ID"]] = to_check[x["ID"]] + x["LENGTHKM"]
-  #   # }
-  #   # else{
-  #   #   to_check[x["ID"]] = x["LENGTHKM"]
-  #   # }
-  # })
   if(distance == 0){
-    return(to_check)
+    nodes = cbind(names(to_check), to_check)
+    rownames(nodes) = c(1:nrow(nodes))
+    colnames(nodes) = c("PERMANENT_", "LENGTHKM")
+    return(nodes)
   }
-  #for(i in 1:(distance - 1)){
   while(1){
     next_check = c()
-    to_check = to_check[to_check != 0]
-    nodes = c(nodes, names(to_check))
+    #to_check = to_check[to_check != 0]
+    nodes = rbind(nodes, cbind(names(to_check), to_check))
     for(j in 1:length(to_check)){
         n = neighbors(g, names(to_check)[j], direction)
+        n$LENGTHKM[is.na(n$LENGTHKM)] = 0
         next_check_tmp = n$LENGTHKM + to_check[j]
         names(next_check_tmp) = n$ID
         next_check = c(next_check, next_check_tmp)
     }
     for(j in names(next_check)){
       if(next_check[j] > distance){
-        nodes = c(nodes, names(to_check))
+        nodes = rbind(nodes, cbind(names(to_check), to_check))
+        rownames(nodes) = c(1:nrow(nodes))
+        colnames(nodes) = c("PERMANENT_", "LENGTHKM")
         return(nodes)
       }
     }
