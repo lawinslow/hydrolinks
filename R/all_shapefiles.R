830b4703e7d8ab5acf38570b6ee2da9ffdf26f0c
Luke Winslow <lawinslow@gmail.com>
2017-08-25 12:34:47 -0400

    Some file and name cleanup
     - Also fixed all_shapefile function

---
+++ R/all_shapefiles.R
@@ -0,0 +1,52 @@
+#' @title Return path to all shapefiles
+#'
+#' @description
+#' Returns list of paths to shapefiles for use in custom processing.
+#'
+#'
+#' @param check_dl If TRUE, checks to ensure all files for that dataset have been downloaded.
+#' This check takes some time (~30 seconds) to check all files (and much longer to dowload if necessary)
+#'
+#'
+#' @export
+all_shapefiles = function(check_dl=FALSE, dataset = c("nhdh", "hydrolakes", "nhdplusv2")){
+
+  dataset = match.arg(dataset)
+  dl_file = ""
+  id_column = ""
+  bbdf = NULL
+  bbdf_waterbody = NULL
+  if(tolower(dataset) == "nhdh"){
+    load(file=system.file('extdata/nhd_bb_cache_projected.Rdata', package='hydrolinks'))
+    dl_file = "extdata/nhdh.csv"
+    id_column = "PERMANENT_"
+    wbd_bb = bbdf
+  }
+  else if(tolower(dataset) == "hydrolakes"){
+    load(file=system.file('extdata/hydrolakes_bb_cache_projected.Rdata', package='hydrolinks'))
+    dl_file = "extdata/hydrolakes.csv"
+    id_column = "Hylak_id"
+    wbd_bb = bbdf
+  }
+  else if(tolower(dataset) == "nhdplusv2"){
+    load(file=system.file('extdata/nhdplus_waterbody_bb_cache.rdata', package='hydrolinks'))
+    dl_file = "extdata/nhdplusv2.csv"
+    id_column = "COMID"
+    wbd_bb = bbdf_waterbody
+  }
+
+  if(check_dl){
+    check_dl_file(system.file(dl_file, package = "hydrolinks"))
+  }
+  shapefile_name = ""
+  if(tolower(dataset) == "nhdh" || tolower(dataset) == "nhdplusv2"){
+    shapefile_name = "NHDWaterbody_projected.shp"
+  }
+  else if(tolower(dataset) == "hydrolakes"){
+    shapefile_name = "HydroLAKES_polys_v10_projected.shp"
+  }
+
+  files = file.path(local_path(), "unzip", read.csv(system.file(dl_file, package = "hydrolinks"))$filename, shapefile_name)
+
+  return(files)
+}
---
+++ R/link_to_waterbodies.R
@@ -0,0 +1,101 @@
+#' @title Link geopoints to Waterbodies
+#'
+#' @description Link geopoints to waterbodies in a geospatial dataset.
+#'
+#' @param lats Vector of point latitudes
+#' @param lons Vector of point longitudes
+#' @param ids Vector of point identifiers (string or numeric)
+#' @param dataset Character name of dataset to link against. Can be either "nhdh", "hydrolakes", or "nhdplusv2"
+#' @param buffer Numeric width of polygon buffer in m
+#'
+#'
+#' @return Water body permanent IDs
+#'
+#' @import rgdal
+#' @importFrom rgeos gBuffer
+#' @import sp
+#'
+#' @export
+link_to_waterbodies = function(lats, lons, ids, dataset = c("nhdh", "hydrolakes", "nhdplusv2"), buffer = 0){
+  dataset = match.arg(dataset)
+  dl_file = ""
+  id_column = ""
+  bbdf = NULL
+  bbdf_waterbody = NULL
+  if(tolower(dataset) == "nhdh"){
+    load(file=system.file('extdata/nhd_bb_cache_projected.Rdata', package='hydrolinks'))
+    dl_file = "extdata/nhdh.csv"
+    id_column = "PERMANENT_"
+    wbd_bb = bbdf
+  }
+  else if(tolower(dataset) == "hydrolakes"){
+    load(file=system.file('extdata/hydrolakes_bb_cache_projected.Rdata', package='hydrolinks'))
+    dl_file = "extdata/hydrolakes.csv"
+    id_column = "Hylak_id"
+    wbd_bb = bbdf
+  }
+  else if(tolower(dataset) == "nhdplusv2"){
+    load(file=system.file('extdata/nhdplus_waterbody_bb_cache.rdata', package='hydrolinks'))
+    dl_file = "extdata/nhdplusv2.csv"
+    id_column = "COMID"
+    wbd_bb = bbdf_waterbody
+  }
+
+
+  sites = data.frame(lats, lons, ids)
+  not_na = which(!is.na(sites$lats) & !is.na(sites$lons))
+
+  xy = cbind(sites$lons, sites$lats)
+
+  pts = SpatialPoints(xy[not_na, , drop=FALSE], proj4string=CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
+  pts = spTransform(pts, CRS("+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=23 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs"))
+
+  res   = list()
+
+  xmin = xmax = ymin = ymax = NULL
+  for(i in 1:nrow(sites)){
+    res[[i]] = subset(wbd_bb, xmin <= pts@coords[i,1] & xmax >= pts@coords[i,1] & ymin <= pts@coords[i,2] & ymax >= pts@coords[i,2])
+  }
+
+  to_check = unique(do.call(rbind, res))
+
+
+  match_res = list()
+
+  if(nrow(to_check) == 0){
+      ret = data.frame(MATCH_ID = sites$ids)
+      ret$PERMANENT_ID = NA
+      return(ret)
+  }
+
+  #TODO: Finish this
+  for(i in 1:nrow(to_check)){
+    #get nhd layer
+    check_dl_file(system.file(dl_file, package = "hydrolinks"), to_check[i, 'file'])
+
+    shapefile_name = ""
+    if(tolower(dataset) == "nhdh" || tolower(dataset) == "nhdplusv2"){
+      shapefile_name = "NHDWaterbody_projected.shp"
+    }
+    else if(tolower(dataset) == "hydrolakes"){
+      shapefile_name = "HydroLAKES_polys_v10_projected.shp"
+    }
+
+    nhd       = readOGR(file.path(local_path(), "unzip", to_check[i,'file'], shapefile_name), stringsAsFactors=FALSE)
+
+    if(buffer > 0){
+      nhd = gBuffer(nhd, byid = TRUE, width = buffer)
+    }
+
+    ids = rep(NA, length(sites$lats))
+
+    matches = over(pts, nhd, fn = NULL, returnList = FALSE)
+
+    matches$MATCH_ID = sites$ids[not_na]
+    match_res[[i]] = matches
+  }
+
+  unique_matches = unique(bind_rows(match_res))
+  #return matches that have non-NA value id
+  return(unique_matches[!is.na(unique_matches[,id_column]),])
+}
