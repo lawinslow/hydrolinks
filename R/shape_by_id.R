7ab2f92ab46d33b2278befcf10505df58f7ec095
Tobi Hahn <rainshapes@gmail.com>
2017-08-29 13:50:11 -0400

    fixed shapefile linking, improved matching by id

--- R/shape_by_id.R
+++ R/shape_by_id.R
@@ -4,83 +4,55 @@
 #' other arguments will be used to filter. Arguments are checked in order: PERMANENT_match, GNIS_ID_match,
 #' GNIS_NAME_match, REACHCODE_match.
 #'
-#' @param PERMANENT_match filter using PERMANENT_
-#' @param GNIS_ID_match filter using GNIS_ID
-#' @param GNIS_NAME_match filter using GNIS_NAME
-#' @param REACHCODE_match filer using REACHCODE
+#' @param feature_type name of feature layer to match. The hydrolakes dataset does not include a flowline layer.
+#' @param dataset name of dataset to use for matching.
+#' @param match_column index containing match ids. Columns indexed by dataset:
+#' \tabular{lll}{
+#' nhdh \tab nhdplusv2 \tab hydrolakes \cr
+#' PERMANENT_ \tab COMID \tab Hylak_id \cr
+#' GNIS_ID \tab GNIS_ID \tab Lake_name \cr
+#' GNIS_NAME \tab GNIS_NAME \cr
+#' REACHCODE \tab REACHCODE
+#' }
 #'
-#' @return SpatialPointsDataFrame containing polygons with associated IDs.
+#' @return simple features object containing polygons with associated IDs.
 #' @import dplyr
-#' @import rgdal
+#' @import sf
 #'
 #' @export
 
-
-waterbody_shape_by_id = function(PERMANENT_match = NULL, GNIS_ID_match = NULL, GNIS_NAME_match = NULL, REACHCODE_match = NULL){
-  check_dl_file(system.file("extdata/id_db.csv", package = "hydrolinks"), fname = "waterbody_ids.zip")
-  id_db = src_sqlite(file.path(local_path(), "unzip", "waterbody_ids.zip", "waterbody_ids.sqlite3"))
+get_shape_by_id = function(feature_type = c("flowline", "waterbody"), dataset = c("nhdh", "nhdplusv2", "hydrolakes"), match_column, match_id){
+  feature_type = match.arg(feature_type)
+  dataset = match.arg(dataset)
+  db_name = paste0(dataset, "_", feature_type, "_ids")
+  check_dl_file(system.file("extdata/id_db.csv", package = "hydrolinks"), fname = paste0(db_name, ".zip"))
+  id_db = src_sqlite(file.path(local_path(), "unzip", paste0(db_name, ".zip"), paste0(db_name, ".sqlite3")))
+  id_db = src_sqlite(paste0(db_name, ".sqlite3"))
   shape = id_db %>%
-    tbl("waterbody_ids")
-
-  PERMANENT_ = GNIS_ID = GNIS_NAME = REACHCODE = NULL
-
-  if(!is.null(PERMANENT_match))
-    shape = filter(shape, PERMANENT_ %in% PERMANENT_match)
-  else if(!is.null(GNIS_ID_match))
-    shape = filter(shape, GNIS_ID %in% GNIS_ID_match)
-  else if(!is.null(GNIS_NAME_match))
-    shape = filter(shape, GNIS_NAME %in% GNIS_NAME_match)
-  else if(!is.null(REACHCODE_match))
-    shape = filter(shape, REACHCODE %in% GNIS_NAME_match)
-  shape = collect(shape)
-  files = unique(shape$file)
-  shapes = list()
-  if(length(files) > 0){
-    for(i in 1:length(files)){
-      check_dl_file(system.file("extdata/nhdh.csv", package="hydrolinks"), fname = files[i])
-      shapes[[i]] = readOGR(file.path(local_path(), "unzip", files[i], "NHDWaterbody.shp"), stringsAsFactors=FALSE)
-    }
-    if(length(shapes) > 1)
-      return(do.call(rbind, shapes))
-    else
-      return(shapes[[1]])
-  }
-}
-
-
-#' @title Link IDs to flowline shapefiles
-#'
-#' @description Get shapefiles containing flowlines with specified IDs.
-#'
-#' @param PERMANENT_match filter using PERMANENT_
-#'
-#' @return SpatialPointsDataFrame containing polygons with associated IDs.
-#' @import dplyr
-#' @import rgdal
-#'
-#' @export
-
-flowline_shape_by_id = function(PERMANENT_match){
-  check_dl_file(system.file("extdata/id_db.csv", package = "hydrolinks"), fname = "flowline_ids.zip")
-  id_db = src_sqlite(file.path(local_path(), "unzip", "flowline_ids.zip", "flowline_ids.sqlite3"))
-
-  PERMANENT_ = NULL
-
-  shape = id_db %>%
-    tbl("flowline_ids") %>%
-    filter(PERMANENT_ %in% PERMANENT_match) %>%
+    tbl('id_lookup') %>%
+    filter_(paste0(match_column, " %in% ", match_id)) %>%
     collect()
-  shapes = list()
   files = unique(shape$file)
+  shapes = list()
+  
+  shapefile_name = ""
+  if(dataset == "nhdh" || dataset == "nhdplusv2"){
+    if(feature_type == "waterbody"){
+      shapefile_name = "NHDWaterbody_projected.shp"
+    }
+    else{
+      shapefile_name = "NHDFlowline_projected.shp"
+    }
+  }
+  else if(dataset == "hydrolinks"){
+    shapefile_name = "HydroLAKES_polys_v10_projected.shp"
+  }
+  
   if(length(files) > 0){
     for(i in 1:length(files)){
-      check_dl_file(system.file("extdata/nhdh.csv", package="hydrolinks"), fname = files[i])
-      shapes[[i]] = readOGR(file.path(local_path(), "unzip", files[i], "NHDFlowline_projected.shp"), stringsAsFactors=FALSE)
+      check_dl_file(system.file(paste0("extdata/", dataset, ".csv"), package = "hydrolinks"), fname = files[i])
+      shapes[[i]] = st_read(file.path(local_path(), "unzip", files[i], shapefile_name))
     }
-    if(length(shapes) > 1)
-      return(do.call(rbind, shapes))
-    else
-      return(shapes[[1]])
   }
   if(length(shapes) > 1)
     return(do.call(rbind, shapes))
