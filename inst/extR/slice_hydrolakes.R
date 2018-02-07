ec1eeca0ae1e37197c35bc8eca13f1c9ecc63cd7
Luke Winslow <lawinslow@gmail.com>
2018-02-07 14:39:13 -0500

    Improve hydrolakes slicing and linking
     - Eliminate hard-coded indices
     - Ensure even slicing
     - Reduce need for working path jumping
     - (build_id_table still needs work), it assumes two inputs are in the
     same order, makes me nervous

--- inst/extR/slice_hydrolakes.R
+++ inst/extR/slice_hydrolakes.R
@@ -3,23 +3,23 @@ source("R/AAA.R")
 source("inst/extR/general_functions.R")
 
 hydrolakes_path = "D:/hydrolakes/HydroLAKES_polys_v10_shp"
-hydrolakes_path = "B:/big_data/HydroLAKES_polys_v10_shp"
+#hydrolakes_path = "B:/big_data/HydroLAKES_polys_v10_shp"
 
 id_table_output_path = "D:/hydrolinks_tables"
-id_table_output_path = "B:/big_data/hydrolinks_tables"
+#id_table_output_path = "B:/big_data/hydrolinks_tables"
 
 hydrolakes = st_read(file.path(hydrolakes_path, "HydroLAKES_polys_v10.shp"))
 centroids = st_centroid(hydrolakes)
 hydrolakes$centroid.x = st_coordinates(centroids)[,"X"]
 hydrolakes = hydrolakes[order(hydrolakes$centroid.x), ]
 
+#setup the slices, evenly distributed across the whole dataset
+nslices = 50
+indx = floor(seq(1, nrow(hydrolakes), length.out = (nslices+1)))
+
 bboxes = list()
-for(i in c(1:50)){
-  if(i < 50){
-    slice = hydrolakes[c((28000 * (i-1) + 1):(28000*i)),]
-  }else{
-    slice = hydrolakes[c(1372001:1427688),]
-  }
+for(i in 1:(length(indx)-1)){
+  slice = hydrolakes[indx[i]:indx[i+1], ]
   dir.create(file.path(hydrolakes_path, paste0("hydrolakes_", i)))
   slice = st_transform(slice, nhd_projected_proj)
   centroids = st_centroid(slice)
@@ -34,14 +34,14 @@ bbdf = do.call(rbind, bboxes)
 save(bbdf, file='inst/extdata/hydrolakes_bb_cache.Rdata')
 
 dir.create(file.path(hydrolakes_path, "zip"))
-output_zip = file.path(hydrolakes_path, "zip", paste0("hydrolakes_", c(1:50), ".zip"))
-for(i in 1:50){
-  setwd(file.path(hydrolakes_path, paste0("hydrolakes_", i)))
-  zip(output_zip[i], Sys.glob("*_projected.*"))
+output_zip = file.path(hydrolakes_path, "zip", paste0("hydrolakes_", 1:nslices, ".zip"))
+for(i in 1:nslices){
+  tozip = Sys.glob(file.path(hydrolakes_path, paste0("hydrolakes_", i), '*'))
+  zip(output_zip[i], files=tozip, flags='-j')
 }
 
 setwd(hydrolakes_path)
 build_id_table(bbdf, "HydroLAKES_polys_v10_projected.shp", file.path(id_table_output_path, "hydrolakes_waterbody_ids.sqlite3"), c("Hylak_id", "Lake_name"), paste0("hydrolakes_", c(1:50)))
 
 processed_shapes = gen_upload_file(output_zip, "hydrolinks/0.7/hydrolakes")
-write.csv(processed_shapes, "inst/extdata/hydrolakes.csv")
+write.csv(processed_shapes, "inst/extdata/hydrolakes.csv", row.names=FALSE)
