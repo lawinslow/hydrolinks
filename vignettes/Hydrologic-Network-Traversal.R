0d1360c4222be6e3dae980862a91e2015606725e
Luke Winslow <lawinslow@gmail.com>
2018-04-12 11:15:50 -0400

    working on fixing vignette building on CRAN

--- vignettes/Hydrologic-Network-Traversal.R
+++ vignettes/Hydrologic-Network-Traversal.R
@@ -1,36 +1,41 @@
 ## ----setup, include = FALSE----------------------------------------------
 knitr::opts_chunk$set(
   collapse = TRUE,
-  comment = "#>"
-)
+  comment = "#>")
+
+is_check <- ("CheckExEnv" %in% search()) || any(c("_R_CHECK_TIMINGS_",
+             "_R_CHECK_LICENSE_") %in% names(Sys.getenv()))
+knitr::opts_chunk$set(eval = !is_check)
+
 
 ## ----eval=TRUE, echo=TRUE, warning=FALSE, include=FALSE------------------
 library(hydrolinks)
 library(sf)
+hydrolinks::cache_set_dir(temppath = TRUE)
 linked_wb = link_to_waterbodies(39.086637, -120.052634, 'lakeobs1', dataset='nhdh')
 
 ## ----eval=TRUE, echo=FALSE-----------------------------------------------
 print(linked_wb)
 
 ## ----eval=TRUE, echo=TRUE, message=FALSE, warning=FALSE, results='hide'----
-lake_poly = get_shape_by_id(linked_wb$PERMANENT_, dataset = 'nhdh', feature_type = 'waterbody')
+lake_poly = get_shape_by_id(linked_wb$permanent_, dataset = 'nhdh', feature_type = 'waterbody')
 #Simple quick viz of polygon
 plot(st_geometry(lake_poly), main='Lake Tahoe', col='Dodgerblue')
 
 
 ## ----eval=TRUE, echo=TRUE, message=FALSE, warning=FALSE, results='hide'----
 
-upstream = traverse_flowlines(50, linked_wb$PERMANENT_, direction = 'in')
-upstream_shp = get_shape_by_id(upstream$PERMANENT_, dataset = 'nhdh', feature_type = 'flowline')
+upstream = traverse_flowlines(50, linked_wb$permanent_, direction = 'in')
+upstream_shp = get_shape_by_id(upstream$permanent_, dataset = 'nhdh', feature_type = 'flowline')
 plot(st_geometry(upstream_shp), col='palegreen')
 plot(st_geometry(lake_poly), main='Lake Tahoe', col='Dodgerblue', add=TRUE)
 
 
 ## ----eval=TRUE, echo=TRUE, message=FALSE, warning=FALSE, results='hide'----
 
-downstream = traverse_flowlines(2000, linked_wb$PERMANENT_, direction = 'out')
-downstream_shp = get_shape_by_id(downstream$PERMANENT_, dataset = 'nhdh', feature_type = 'flowline')
-downstream_lk_shp = get_shape_by_id(downstream$PERMANENT_, dataset = 'nhdh', feature_type = 'waterbody')
+downstream = traverse_flowlines(2000, linked_wb$permanent_, direction = 'out')
+downstream_shp = get_shape_by_id(downstream$permanent_, dataset = 'nhdh', feature_type = 'flowline')
+downstream_lk_shp = get_shape_by_id(downstream$permanent_, dataset = 'nhdh', feature_type = 'waterbody')
 plot(st_geometry(downstream_shp), col='palegreen')
 plot(st_geometry(downstream_lk_shp), main='Lake Tahoe', col='Dodgerblue', add=TRUE)
 plot(st_geometry(lake_poly), main='Lake Tahoe', col='Dodgerblue', add=TRUE)
@@ -39,12 +44,12 @@ plot(st_geometry(lake_poly), main='Lake Tahoe', col='Dodgerblue', add=TRUE)
 ## ----eval=TRUE, echo=TRUE, message=FALSE, warning=FALSE, results='hide'----
 id = link_to_waterbodies(43.112449, -89.429409, 'mendota')
 
-hmm = traverse_flowlines(100, id$PERMANENT_, "in")
+hmm = traverse_flowlines(100, id$permanent_, "in")
 
-fls = get_shape_by_id(hmm$PERMANENT_, feature_type = 'flowline', dataset='nhdh')
-wbs = get_shape_by_id(hmm$PERMANENT_, feature_type = 'waterbody', dataset='nhdh')
+fls = get_shape_by_id(hmm$permanent_, feature_type = 'flowline', dataset='nhdh')
+wbs = get_shape_by_id(hmm$permanent_, feature_type = 'waterbody', dataset='nhdh')
 
-wb   = get_shape_by_id(id$PERMANENT_, feature_type = 'waterbody')
+wb   = get_shape_by_id(id$permanent_, feature_type = 'waterbody')
 
 plot(st_geometry(fls), col='green')
 plot(st_geometry(wb), col='orange', add=TRUE)
--- vignettes/wqp_lake_linking_example.R
+++ vignettes/wqp_lake_linking_example.R
@@ -1,13 +1,19 @@
 ## ----setup, include=FALSE------------------------------------------------
 knitr::opts_chunk$set(echo = TRUE, cache=TRUE)
 
+is_check <- ("CheckExEnv" %in% search()) || any(c("_R_CHECK_TIMINGS_",
+             "_R_CHECK_LICENSE_") %in% names(Sys.getenv()))
+knitr::opts_chunk$set(eval = !is_check)
+
+
 ## ----packages_sites, eval=TRUE, echo=TRUE, warning=FALSE, include=TRUE, message=FALSE----
 library(dataRetrieval)
 library(hydrolinks)
 library(USAboundaries)
-library(ggplot2)
-library(gridExtra)
+#library(ggplot2)
+#library(gridExtra)
 library(sf)
+hydrolinks::cache_set_dir(temppath = TRUE)
 
 lsites = whatWQPsites(siteType ="Lake, Reservoir, Impoundment", statecode='US:55')
 
@@ -31,21 +37,40 @@ kable(tbl, 'html') %>%   kable_styling()
 wi = USAboundaries::us_boundaries()[USAboundaries::us_boundaries()$geoid %in% c('55'),]
 all_points = st_as_sf(lsites, coords = c("LongitudeMeasure", "LatitudeMeasure"), crs = '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs')
 
+par(mfrow=c(2,2), mar=c(0,0,0,0))
+plot(st_geometry(wi))
+plot(st_geometry(all_points), col='darkorange2', add=TRUE, pch=16)
+legend('topright', legend=' ', title='a) all WQP points', bty='n')
 
-gall = ggplot(all_points) + geom_sf(data=wi, fill='grey') +
-  geom_sf(size=0.5, color='darkorange2') + ylim(42, 47) + xlim(-93, -86.5) +
-  ggtitle('a) all WQP points')
+pippoints = subset(all_points, MonitoringLocationIdentifier %in% linked_lakes$MATCH_ID)
+plot(st_geometry(wi))
+plot(st_geometry(pippoints), col='darkorchid3', add=TRUE, pch=16)
+legend('topright', legend=' ', title='b) all linked point-in-poly', bty='n')
 
-glink = ggplot(subset(all_points, MonitoringLocationIdentifier %in% linked_lakes$MATCH_ID)) +
-  geom_sf(data=wi, fill='grey') +
-  geom_sf(size=0.5, color='dodgerblue') + ylim(42, 47) + xlim(-93, -86.5) +
-  ggtitle('b) all linked point-in-poly')
-
-gcent = ggplot(subset(all_points, MonitoringLocationIdentifier %in% linked_lcent$MATCH_ID)) +
-  geom_sf(data=wi, fill='grey') +
-  geom_sf(size=0.5, color='darkorchid3') + ylim(42, 47) + xlim(-93, -86.5) +
-  ggtitle('c) all linked centroid')
+centpoints = subset(all_points, MonitoringLocationIdentifier %in% linked_lcent$MATCH_ID)
+plot(st_geometry(wi))
+plot(st_geometry(centpoints), col='darkorchid3', add=TRUE, pch=16)
+legend('topright', legend=' ', title='c) all linked centroid', bty='n')
 
 
-grid.arrange(gall, glink, gcent, nrow = 2)
+# gall = ggplot(all_points) + 
+#   geom_sf(data=wi, fill='grey') +
+#   geom_sf(size=0.5, color='darkorange2') + ylim(42, 47) + xlim(-93, -86.5) +
+#   ggtitle('a) all WQP points')
+# 
+# glink = ggplot(subset(all_points, MonitoringLocationIdentifier %in% linked_lakes$MATCH_ID)) +
+#   geom_sf(data=wi, fill='grey') +
+#   geom_sf(size=0.5, color='dodgerblue') + ylim(42, 47) + xlim(-93, -86.5) +
+#   ggtitle('b) all linked point-in-poly')
+# 
+# gcent = ggplot(subset(all_points, MonitoringLocationIdentifier %in% linked_lcent$MATCH_ID)) +
+#   geom_sf(data=wi, fill='grey') +
+#   geom_sf(size=0.5, color='darkorchid3') + ylim(42, 47) + xlim(-93, -86.5) +
+#   ggtitle('c) all linked centroid')
+# 
+# 
+# grid.arrange(gall, glink, gcent, nrow = 2)
+
+
+
 
